812eef8ad91325bad7fa5ef1b91fdd591d3b56be
Michael Weiss <michael.weiss@digitalchange.eu>
2026-09-22 12:48:48 +0200

    increase verbosity

--- headlamp.sh
+++ headlamp.sh
@@ -5,6 +5,13 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/headlamp/main
 kubectl -n kube-system create serviceaccount headlamp-admin
 kubectl create clusterrolebinding headlamp-admin --serviceaccount=kube-system:headlamp-admin --clusterrole=cluster-admin
 kubectl patch -n kube-system svc headlamp -p '{"spec":{"type":"NodePort","ports":[{"port":80,"nodePort":30000}]}}'
+echo
+echo "Your ID-Token for Headlamp:"
 kubectl create token headlamp-admin -n kube-system
-sleep 2
-kubectl get -n kube-system svc headlamp
+echo
+echo "Waiting for deployment..."
+sleep 5
+echo "Headlamp deployment information:"
+kubectl get -n kube-system deployment headlamp
+kubectl get -n kube-system pods -l k8s-app=headlamp
+kubectl get -n kube-system service headlamp
